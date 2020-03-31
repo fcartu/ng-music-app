55bcd27fa7e99682ddef48f97f48d899d13a7226
Francisco Cartusciello <fcartu@gmail.com>
2020-03-30 22:17:18 -0500

    implement shared module

---
+++ projects/music-app/src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // Vendor
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    // Vendor
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
