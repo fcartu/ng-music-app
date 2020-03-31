5b919e796d1eca2a9268105df14913ac380dd26d
Francisco Cartusciello <fcartu@gmail.com>
2020-03-30 22:12:46 -0500

    fix error from routerlink

--- projects/music-app/src/app/core/core.module.ts
+++ projects/music-app/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -17,6 +18,7 @@ import { MatListModule } from '@angular/material/list';
     // Vendor
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     LayoutModule,
 
     // Material
